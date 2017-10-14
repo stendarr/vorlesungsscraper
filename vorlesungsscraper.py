03c899b5b2bcf69455df98fc1432a598ef508775
dominik <stendarr@users.noreply.github.com>
2017-10-14 14:25:33 +0200

    now downloads the prof's notes made during the lectures

--- vorlesungsscraper.py
+++ vorlesungsscraper.py
@@ -6,15 +6,15 @@ Make sure you have `requests` and `bs4` installed -> pip install requests/bs4
 
 Check README.md and LICENSE before using this program.
 
-Checklist | Lectures
-----------|----------
-AlgDat    |    n/a
-----------|----------
-DisMat    |    n/a  
-----------|----------
-EProg     |     y   
-----------|----------
-LinAlg    |     y   
+Checklist | Lectures | other
+----------|----------|--------
+AlgDat    |    n/a   |
+----------|----------|--------
+DisMat    |    n/a   |
+----------|----------|--------
+EProg     |     y    |
+----------|----------|--------
+LinAlg    |     y    |  notes made by prof during lecture
 
 '''
 
@@ -45,7 +45,7 @@ directories = ["Vorlesungen/EProg", "Vorlesungen/LineareAlgebra"]
 for i in directories:
     if not os.path.isdir(i):
         os.makedirs(i)
-        print("This folder was generated:   ",i) 
+        print("This folder was generated:   ",i)
     else:
         print("This folder already exists:  ",i)
 print("\n\n")
@@ -68,14 +68,14 @@ for div in soup.find_all('div', class_='newsListBox'):
     eprog_link_dict[eprog_counter] = link
     eprog_date_dict[eprog_counter] = date
     print('['+str(eprog_counter)+']',date,'\n',link,'\n')
-    eprog_counter += 1  
-    
+    eprog_counter += 1
+
 try:
     choice = [int(x) for x in input("Enter numbers of lectures, separated by space (e.g. 0 3 5 7)\nJust press enter if you don't want to download anything\n").split()]
 except:
     print("You done fucked up")
     sys.exit()
-    
+
 print('')
 
 #Get video url from each site choosen
@@ -91,7 +91,7 @@ for c in choice:
     #Let's hope everything is .mp4
 
     print('\n',link)
-    
+
     if os.path.isfile(filename):
         print("---skipped - already exists")
     else:
@@ -119,6 +119,7 @@ la_link = 'http://www.video.ethz.ch/lectures/d-math/2017/autumn/401-0131-00L.htm
 la_link_dict = {}
 la_date_dict = {}
 la_counter = 0
+la_links = []
 
 request = Request(la_link, headers={'User-Agent': user_agent})
 soup = BeautifulSoup(urllib.request.urlopen(request), 'html.parser')
@@ -131,13 +132,12 @@ for div in soup.find_all('div', class_='newsListBox'):
     print('['+str(la_counter)+']',date,'\n',link,'\n')
     la_counter += 1
 
-
 try:
     choice = [int(x) for x in input("Enter numbers of lectures, separated by space (e.g. 0 3 5 7)\nJust press enter if you don't want to download anything\n").split()]
 except:
     print("You done fucked up")
     sys.exit()
-    
+
 print('')
 
 #open every chosen lecture
@@ -170,8 +170,44 @@ for c in choice:
         download_counter += 1
         link_list.append(link)
         filename_list.append(filename)
-        
 
+#LinAlg notes
+if input("Do you want to download the LinAlg notes made during the lecture? (y/n)\n").lower() == "y":
+    la_link = 'http://igl.ethz.ch/teaching/linear-algebra/la2017/'
+    request = Request(la_link, headers={'User-Agent' : user_agent})
+    soup = BeautifulSoup(urllib.request.urlopen(request), 'html.parser')
+
+    soup2 = str(soup).partition('ersicht der Vorlesung</h3>')[-1]
+    soup2 = soup2.rpartition('<a class="a-int" href="http://www.video.ethz.ch/l')[0]
+    soup2 = BeautifulSoup(soup2, 'html.parser')
+
+    for link in soup2.find_all('a'):
+        la_links.append(link.get('href'))
+
+    for link in la_links:
+        split = urllib.parse.urlsplit(link)
+        filename = 'Vorlesungen/LineareAlgebra/'+split.path.split('/')[-1]
+        link = 'http://igl.ethz.ch/teaching/linear-algebra/la2017/'+link
+        print(link)
+        link_counter += 1
+        if os.path.isfile(filename):
+            print("---skipped - already exists")
+        else:
+            r = requests.get(link, allow_redirects=False, headers={'User-Agent' : user_agent})
+            with open(filename, 'wb') as f:
+                for chunk in r.iter_content(1024):
+                    f.write(chunk)
+            print("---downloaded")
+            download_counter += 1
+            link_list.append(link)
+            filename_list.append(filename)
+#end Linalg notes
+
+print()
 print(la_counter+eprog_counter,"Files Found and",download_counter,"Files downloaded")
+if len(filename_list) != 0:
+    print("Notes downloaded into: ")
+    for fn in filename_list:
+        print(fn)
 if platform == "win32":
     input('\nEOF') #Just so Windows users don't get butthurt about not seeing the output
